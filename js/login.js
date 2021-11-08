07e8cc8cc62dc63fbff7f0dcb35d8f0ec537e7bd
remyagrv <93488176+remyagrv@users.noreply.github.com>
2021-11-08 23:58:34 +0530

    vali

--- js/login.js
+++ js/login.js
@@ -7,13 +7,33 @@ function myFunction() {
   }
 }
 function validate(){  
-  // let username = document.getElementById("usernm");  
-  let password = document.getElementById("myInput");  
-    if(password.length<6){  
-    alert("Password must be at least 6 characters long.");  
-    return false; 
-    } 
-    else{
-      return true;
+  let email = document.getElementById("email");  
+  let password = document.getElementById("myInput"); 
+  let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; 
+  let pwdreg = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+    
+  if(password.value.length < 8)
+    {
+      alert("Password must be at least 8 characters long."); 
+      return false; 
     }
+  else if ((regex.test(email.value))&&(pwdreg.test(password.value)))
+  {
+      
+      return true;
+  } else {
+     alert("Incorrect Email and Password");
+      return false;
+  }
+  
+      
+ 
+  
+    //   else if(pwdreg.test(password.value)){
+    //     alert("Incorrect Password");
+    //     return false;
+    //   }
+    // else{
+    //   return true;
+    // }
   }
