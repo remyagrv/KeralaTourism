40055dc0387294df2d1b1fbe030b86db3dc485e2
remyagrv <93488176+remyagrv@users.noreply.github.com>
2021-11-11 00:06:25 +0530

    signn

--- js/signup.js
+++ js/signup.js
@@ -3,15 +3,14 @@ function registrationValid()
     var regName = /^[a-zA-Z]+$/;
     let regemail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; 
     var phonereg = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-    let pwdreg = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+    
     var Fname = document.getElementById('fname');
     var Lname = document.getElementById('lname');
     let email = document.getElementById("email"); 
     let phno = document.getElementById("phn");
-    let timeout;
-let password = document.getElementsById('password');
-let cpassword = document.getElementsById('password');
-// let strengthBadge = document.getElementById('StrengthDisp');
+    let password = document.getElementById("pass");
+    let cpassword = document.getElementById("cpass");
+    let pwdregu = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
 
     if(!regName.test(Fname.value)){
         alert('Invalid firstname given!');
@@ -25,17 +24,35 @@ let cpassword = document.getElementsById('password');
     }else if(!phonereg.test(phno.value)){
         alert('Invalid Mobile Number!');
         return false;
-
-    }else if((!pwdreg.test(password.value))||(!pwdreg.test(cpassword.value))) {
+    
+    }else if((!pwdregu.test(password.value))||(!pwdregu.test(cpassword.value))){
         alert('Invalid Password!');
         return false;
-
-    } else if(password.value!=cpassword.value) {
-        alert('');
+    }else if(password.value!=cpassword.value){
+        alert('Passwords did not match!');
         return false;
-        
     }
-    else if((regName.test(Fname.value))&&(regName.test(Lname.value))&&(regemail.test(email.value))&&(phonereg.test(phno.value)))
+    else((regName.test(Fname.value))&&(regName.test(Lname.value))&&(regemail.test(email.value))&&(phonereg.test(phno.value)))
+    {
     return true;
 }
 
+
+}
+function passwordChanged() {
+    var strength = document.getElementById('strength');
+    var strongRegex = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})');
+    var mediumRegex = new RegExp('((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,}))|((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))');
+    
+    var pwd = document.getElementById("pass");
+    if (pwd.value.length == 0) {
+        strength.innerHTML = 'Type Password';
+    
+    } else if (strongRegex.test(pwd.value)) {
+        strength.innerHTML = '<span style="background-color:green" >Strong!</span>';
+    } else if (mediumRegex.test(pwd.value)) {
+        strength.innerHTML = '<span style="background-color:orange">Medium!</span>';
+    } else {
+        strength.innerHTML = '<span style="background-color:red">Weak!</span>';
+    }
+}
