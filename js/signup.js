b86dbf5b9c69ed970b5d80f6daf2365187d8d476
remyagrv <93488176+remyagrv@users.noreply.github.com>
2021-11-10 18:43:51 +0530

    s

--- js/signup.js
+++ js/signup.js
@@ -2,9 +2,17 @@ function registrationValid()
 {
     var regName = /^[a-zA-Z]+$/;
     let regemail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; 
+    var phonereg = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+    let pwdreg = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
     var Fname = document.getElementById('fname');
     var Lname = document.getElementById('lname');
     let email = document.getElementById("email"); 
+    let phno = document.getElementById("phn");
+    let timeout;
+let password = document.getElementsById('password');
+let cpassword = document.getElementsById('password');
+// let strengthBadge = document.getElementById('StrengthDisp');
+
     if(!regName.test(Fname.value)){
         alert('Invalid firstname given!');
         return false;
@@ -14,8 +22,20 @@ function registrationValid()
     }else if(!regemail.test(email.value)){
         alert('Invalid Email!');
         return false;
+    }else if(!phonereg.test(phno.value)){
+        alert('Invalid Mobile Number!');
+        return false;
+
+    }else if((!pwdreg.test(password.value))||(!pwdreg.test(cpassword.value))) {
+        alert('Invalid Password!');
+        return false;
+
+    } else if(password.value!=cpassword.value) {
+        alert('');
+        return false;
+        
     }
-    else if((regName.test(Fname.value))&&(regName.test(Lname.value))&&(regemail.test(email.value)))
+    else if((regName.test(Fname.value))&&(regName.test(Lname.value))&&(regemail.test(email.value))&&(phonereg.test(phno.value)))
     return true;
 }
-    
+
