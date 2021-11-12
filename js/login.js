77941f6266a76c619032e54f610fa01316d57735
remyagrv <93488176+remyagrv@users.noreply.github.com>
2021-11-12 23:11:18 +0530

    new lo

--- js/login.js
+++ js/login.js
@@ -12,33 +12,23 @@ function validate(){
   let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; 
   let pwdreg = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
     
-  
-  
-
-  if ((regex.test(email.value))&&(pwdreg.test(password.value))&&(password.value.length > 8))
-  {
-      
-    return true;
-}
-   
-  else  if(password.value.length < 8)
-  {
+  if (!regex.test(email.value)){
+    alert("Invalid Email"); 
+    return false; 
+  }else if (password.value.length < 8){
     alert("Password must be at least 8 characters long."); 
     return false; 
-  }
-  else {
-     alert("Incorrect Email or Password");
-      return false;
+  }else if (!pwdreg.test(password.value)){
+    alert("Invalid Password"); 
+    return false; 
+  }else{
+    return true;
   }
   
+
+ 
       
  
   
-    //   else if(pwdreg.test(password.value)){
-    //     alert("Incorrect Password");
-    //     return false;
-    //   }
-    // else{
-    //   return true;
-    // }
+    
   }
